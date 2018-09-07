1d68111a6d53be0359714e5fefe68309f820b537
vittominacori <vittoriominacori@gmail.com>
2018-09-07 23:12:16 +0200

    added timelock

---
+++ contracts/token/ForkTimelock.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.24;
+
+import "openzeppelin-solidity/contracts/token/ERC20/TokenTimelock.sol";
+
+
+contract ForkTimelock is TokenTimelock {
+  constructor(
+    ERC20Basic _token,
+    address _beneficiary,
+    uint256 _releaseTime
+  )
+  TokenTimelock(_token, _beneficiary, _releaseTime)
+  public
+  {}
+}
---
+++ test/ForkTimelock.test.js
@@ -0,0 +1,39 @@
+const { latestTime } = require('./helpers/latestTime');
+const { duration } = require('./helpers/increaseTime');
+const { advanceBlock } = require('./helpers/advanceToBlock');
+
+const { shouldBehaveLikeTokenTimelock } = require('./behaviours/TokenTimelock.behaviour');
+
+const BigNumber = web3.BigNumber;
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
+
+const GastroAdvisorToken = artifacts.require('GastroAdvisorToken');
+const ForkTimelock = artifacts.require('ForkTimelock');
+
+contract('ForkTimelock', function ([owner, beneficiary]) {
+  const amount = new BigNumber(100);
+
+  before(async function () {
+    // Advance to the next block to correctly read time in the solidity "now" function interpreted by testrpc
+    await advanceBlock();
+  });
+
+  beforeEach(async function () {
+    this.token = await GastroAdvisorToken.new({ from: owner });
+
+    this.releaseTime = (await latestTime()) + duration.years(1);
+    this.timelock = await ForkTimelock.new(this.token.address, beneficiary, this.releaseTime);
+
+    await this.token.addMinter(owner, { from: owner });
+    await this.token.mint(this.timelock.address, amount, { from: owner });
+    await this.token.finishMinting({ from: owner });
+  });
+
+  context('like a TokenTimelock', function () {
+    shouldBehaveLikeTokenTimelock([owner, beneficiary]);
+  });
+});
